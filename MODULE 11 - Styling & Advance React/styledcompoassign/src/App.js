081629ae0f4e48c0d98f3013a7360dc7fe50fdf4
Vivek Parmar <115700389+vivekparmar2002@users.noreply.github.com>
2023-06-30 12:56:26 +0530

    Add files via upload

---
+++ MODULE 11 - Styling & Advance React/styledcompoassign/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import HomePage from "./HomePage";
+import Header from "./Header";
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <>
+      <HomePage />
+      <Header />
+    </>
+  );
+};
+
+export default App;
+
+const Container = styled.div``;
+const Banner = styled.div`
+  background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
+  height: 100vh;
+  @media (max-width: 640px) {
+    height: 100%;
+    padding-bottom: 2rem;
+  }
+`;
+
+const LightColor = styled.div`
+  background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
+`;
